41c2e98800f9d0e09b8df1a7e5cb3661acec361c
aleksmck <alex.mieciek@gmail.com>
2024-09-12 18:40:51 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 document.getElementById('start-game').addEventListener('click', function() {
-    // Smooth transition to the game board
     document.querySelector('.landing-page').classList.add('fade-out');
     setTimeout(function() {
-        document.querySelector('.landing-page').style.display = 'none'; // Hide the landing page
-        document.querySelector('.game-board').classList.remove('hidden'); // Show the game board
-        document.querySelector('.game-board').classList.add('fade-in');  // Apply fade-in effect
-    }, 600);  // Delay for fade-out animation to complete
+        document.querySelector('.landing-page').style.display = 'none';
+        document.querySelector('.game-board').classList.remove('hidden');
+        document.querySelector('.game-board').classList.add('fade-in');
+    }, 600);
 });
+
 
 
 const gridItems = document.querySelectorAll('.grid-item');
